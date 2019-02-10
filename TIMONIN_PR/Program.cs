9e097ef1c37d1c86aa2c2bfbd675b98f69e7ae29
timon <timon@DESKTOP-22V9SLG>
2019-02-10 07:48:43 +0300

    Добавьте файлы проекта.

---
+++ TIMONIN_PR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIMONIN_PR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
