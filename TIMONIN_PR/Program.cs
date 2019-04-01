56f266498a1ef1b0100806c0e28a1432d0a8cb2d
timon <timon@DESKTOP-ANCORQQ>
2019-04-01 18:58:16 +0300

    Добавлено: Делегаты, Абстрактные классы, интерфейсы

--- TIMONIN_PR/Program.cs
+++ TIMONIN_PR/Program.cs
@@ -3,128 +3,236 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace TIMONIN_PR
 {
-    class Data_base
+    interface IClient_action
     {
-        public static string[] name = { "Селедкин В.В", "Раздолбаев И.А.", "Дурабасов А.А." };
-        public static string[] login = { "User1", "User2", "User3" };
-        public static string[] password = { "1", "2", "3" };
-        public static double[] money = { 1000.0, 2000.0, 3000.0 };
+        void Put_money(double money);
+        void Withdraw_money(double money);
+        void Check_score();
+    }    
+
+    [Serializable]
+    abstract class People
+    {
+        public string name { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public People(string name, string login, string password)
+        {
+            this.name = name;
+            this.login = login;
+            this.password = password;
+        }
     }
 
-    class Clients
-    {
-        private string name;
+    delegate void Info(string message);// Объявляем делегат
+       
+    [Serializable]
+    class Clients:People, IClient_action
+    {           
+        public double money { get; private set;}
         
-        public Clients(int accaunt)
-        {
-            name = Data_base.name[accaunt];            
+        Info some_message;// Создаем переменную делегата
+        Info some_color_message;// Создаем переменную делегата
 
-            string criterion_exit = "N";
-            while (criterion_exit == "N")
+        public Clients(string name, double money, string login, string password) :base(name, login, password)
+        {            
+            this.money = money;                        
+        }       
+
+        // Регистрируем делегаты
+        public void Get_message(Info some_message, Info some_color_message)
+        {
+            this.some_message = some_message;
+            this.some_color_message = some_color_message;
+        }
+
+        public void Check_score()
+        {
+            this.some_message($"На вашем счету {this.money} руб");
+        }
+
+        public void Put_money(double money)
+        {            
+            this.money += money;
+            if (this.some_message != null)
             {
-                Welcome_to_Bank(name);
-                Score_of_accaunt(accaunt);
-                Add_sum(accaunt);
-                Take_of_sum(accaunt);
-                
-                Console.WriteLine("Желаете выйти? (Y/N)");
-                criterion_exit = Console.ReadLine();
-                Console.Clear();
-            }
-           
+                this.some_message($"{name}, Вы положили {money} руб. " +
+                                  $"На вашем счету {this.money} руб. ");
+            }            
         }
 
-        public void Welcome_to_Bank(string name)
+        public void Withdraw_money(double money)
         {
-            Console.Clear();
-            Console.WriteLine($"{name}, добро пожаловать в личный кабинет");
-        }
-
-        public static void Score_of_accaunt(int accaunt)
-        {
-            Console.WriteLine($"На вашем счету: {Data_base.money[accaunt]} руб");
-        }
-
-        public void Add_sum(int accaunt)
-        {
-            Console.WriteLine("Желаете пополнить ваш счет? (Y/N)");
-            string answer = Console.ReadLine();
-            if (answer == "Y")
+            if (this.money >= money)
             {
-                Console.Clear();
-                Console.WriteLine("Введите сумму пополнения:");
-                double sum = Convert.ToDouble(Console.ReadLine());
-                Data_base.money[accaunt] += sum;
-                Score_of_accaunt(accaunt);
-            }
-        }
-
-        public static void Take_of_sum(int accaunt)
-        {
-            Console.WriteLine();
-            Console.WriteLine("Желаете снять деньги? (Y/N)");
-            string answer = Console.ReadLine();
-            if (answer == "Y")
-            {
-                Console.Clear();
-                Console.WriteLine("Введите сумму снятия:");
-                double sum = Convert.ToDouble(Console.ReadLine());
-                
-                if (Data_base.money[accaunt]-sum < 0)
+                this.money -= money;
+                if (this.some_message != null)
                 {
-                    Console.WriteLine("Вы не можете снять больше чем у Вас есть на счету");
-                }
-                else
-                {
-                    Data_base.money[accaunt]  -= sum;
-                    Score_of_accaunt(accaunt);
+                    this.some_message($"{name}, Вы сняли {money} руб." +
+                                            $" На вашем счету {this.money} руб. ");
                 }
             }
+            else
+            {
+                if (this.some_message != null)
+                {
+                    this.some_color_message($"{name}, Вы пытаетесь снять {money} руб." +
+                                      $" На вашем счету {this.money} руб." +
+                                      $" Операция не может быть выполнена.");
+                }                
+            }            
         }
     }
-    
-    
+
     class My_bank
     {        
         public static void Main(string[] args)
-        {                      
+        {               
+            Reade_data_obj(out Clients[] all_clients);
 
-            int accaunt=Authorization();
-            Clients user_of_bank = new Clients(accaunt);
-            Thank();
-            Console.ReadLine();            
-        }              
-        
-        public static int Authorization()
-        {            
+            if (all_clients.Length == 0)// TO DO возможно, имеет смысл делать класс админ который будет это делать
+            {
+                Console.WriteLine("Создание аккаунтов....");
+                Console.WriteLine("Аккаунты созданы");
+                Console.WriteLine("Нажмите любую кнопку");
+                Console.ReadLine();
+                Console.Clear();
+                all_clients=Create_clients();// тут описаны все аккаунты 
+                Save_data_obj(all_clients);
+                Reade_data_obj(out all_clients);                
+            }
+
+            int id = Authorization(all_clients);
+            Client_Operation(all_clients[id]);            
+            Save_data_obj(all_clients);            
+            Console.ReadLine();
+         
+        }
+
+        private static void Show_Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void Show_Color_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void Save_data_obj(object data_obj)
+        {
+            // создаем объект BinaryFormatter
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, data_obj);                
+            }
+        }
+
+        public static  void Reade_data_obj(out Clients[] all_clients)
+        {
+            // создаем объект BinaryFormatter
+            BinaryFormatter formatter = new BinaryFormatter();
+            
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                try
+                {                    
+                    Clients[] deserilizePeople = (Clients[])formatter.Deserialize(fs);
+                    all_clients= new Clients[deserilizePeople.Length];
+                    int i = 0;
+                    foreach (Clients p in deserilizePeople)
+                    {
+                        all_clients[i] = p;
+                        i++;
+                    }                    
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка чтения базы данных");
+                    Console.WriteLine("Не создано ни одного аккаунта");
+                    all_clients = new Clients[0];                
+                }
+            }           
+        }
+
+        public static void Client_Operation(Clients user)
+        {
+            user.Get_message(Show_Message, Show_Color_Message);// какой метод будет выполнятся в классе
+            Console.WriteLine($"{user.name}, добро пожаловать в личный кабинет");
+            while (true)
+            {
+                Console.WriteLine("Сделайте Ваш выбор");
+                Console.WriteLine("1.Положить деньги на счет.");
+                Console.WriteLine("2.Снять деньги.");
+                Console.WriteLine("3.Проверить счет.");
+                int choise = Convert.ToInt32(Console.ReadLine());
+
+                if (choise == 1)
+                {
+                    Console.WriteLine("Какую сумму вы хотите положить");
+                    double money = Convert.ToDouble(Console.ReadLine());
+                    user.Put_money(money);
+                }
+
+                if (choise == 2)
+                {
+                    Console.WriteLine("Какую сумму вы хотите снять");
+                    double money = Convert.ToDouble(Console.ReadLine());
+                    user.Withdraw_money(money);
+                }
+
+                if (choise == 3)
+                {                    
+                    user.Check_score();
+                }
+                Console.WriteLine("Завершить обслуживание (Y) для продолжения нажать любую кнопку");
+                string answer = Console.ReadLine();
+                if (answer == "Y")
+                {
+                    break;
+                }
+            }
+        }
+
+        public static Clients[] Create_clients()
+        {
+            Clients user_1 = new Clients("Иванов", 1000, "Иванов_log", "123");
+            Clients user_2 = new Clients("Петров", 1000, "Петров_log", "123");
+            Clients user_3 = new Clients("Сидоров", 1000, "Сидоров_log", "123");
+            Clients[] all_clients = new Clients [] { user_1, user_2, user_3 };
+            return all_clients;
+        }
+
+        public static int Authorization(Clients[] all_clients)
+        {
             while (true)
             {
                 Console.WriteLine("Введите ваш логин");
                 string write_login = Console.ReadLine();
                 Console.WriteLine("Введите ваш пароль");
                 string write_password = Console.ReadLine();
-                for (int account = 0; account < Data_base.login.Length; account++)
+                for (int account = 0; account < all_clients.Length; account++)
                 {
-                    if (write_login == Data_base.login[account] && write_password == Data_base.password[account])
+                    if (write_login == all_clients[account].login && write_password == all_clients[account].password)
                     {
                         return account;
-                    }                    
+                    }
                 }
                 Console.WriteLine($"Пароль или логин введен неверно");
                 Console.WriteLine($"Пожалуйста, повторите попытку");
                 Console.WriteLine($"Для продолжения нажимте ENTER...");
                 Console.ReadLine();
                 Console.Clear();
-            }      
-        }      
-
-        public static void Thank()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Спасибо, что выбрали банк  «Надежный»");
+            }
         }
     }
 }
